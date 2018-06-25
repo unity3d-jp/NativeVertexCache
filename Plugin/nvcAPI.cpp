2da495e3c18538733ed5102b7ae6691807ad9bab
i-saint <saint.skr@gmail.com>
2018-06-25 20:57:39 +0200

    add nvcAPI.{h,cpp}

---
+++ Plugin/nvcAPI.cpp
@@ -0,0 +1,68 @@
+#include "Plugin/PrecompiledHeader.h"
+#include "Plugin/InputGeomCache.h"
+#include "Plugin/OutputGeomCache.h"
+#include "Plugin/nvcAPI.h"
+
+namespace nvc {
+
+// std::copy can be extremely slow in some platform/compiler. this is faster altenative.
+template<class T>
+static inline void copy(const std::vector<T>& src, T *dst)
+{
+    std::memcpy(dst, src.data(), sizeof(T) * src.size());
+}
+
+} // namespace nvc
+
+
+nvcAPI int nvcGetVertexCount(nvc::OutputGeomCache *self)
+{
+    return self ? (int)self->points.size() : 0;
+}
+
+nvcAPI int nvcGetIndexCount(nvc::OutputGeomCache *self)
+{
+    return self ? (int)self->indices.size() : 0;
+}
+
+nvcAPI void nvcFillIndices(nvc::OutputGeomCache *self, int *dst)
+{
+    if (self) {
+        nvc::copy(self->indices, dst);
+    }
+}
+
+nvcAPI void nvcFillPoints(nvc::OutputGeomCache *self, nvc::float3 *dst)
+{
+    if (self) {
+        nvc::copy(self->points, dst);
+    }
+}
+
+nvcAPI void nvcFillNormals(nvc::OutputGeomCache *self, nvc::float3 *dst)
+{
+    if (self) {
+        nvc::copy(self->normals, dst);
+    }
+}
+
+nvcAPI void nvcFillTangents(nvc::OutputGeomCache *self, nvc::float4 *dst)
+{
+    if (self) {
+        nvc::copy(self->tangents, dst);
+    }
+}
+
+nvcAPI void nvcFillUVs(nvc::OutputGeomCache *self, nvc::float2 *dst)
+{
+    if (self) {
+        nvc::copy(self->uvs, dst);
+    }
+}
+
+nvcAPI void nvcFillColors(nvc::OutputGeomCache *self, nvc::float4 *dst)
+{
+    if (self) {
+        nvc::copy(self->colors, dst);
+    }
+}
---
+++ Plugin/nvcAPI.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifdef _WIN32
+    #define nvcAPI extern "C" __declspec(dllexport)
+#else
+    #define nvcAPI extern "C"
+#endif
