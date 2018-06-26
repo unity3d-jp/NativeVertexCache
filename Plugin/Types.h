c23da5b4b87ffe2974e73a5b7a32d64b397e3b27
i-saint <saint.skr@gmail.com>
2018-06-26 11:19:07 +0200

    implement float <-> half conversion

--- Plugin/Types.h
+++ Plugin/Types.h
@@ -1,16 +1,20 @@
 #pragma once
+#include <cstdint>
 
 namespace nvc {
 
 using time_t = float;
 
 
+// note: this half doesn't care about Inf nor NaN. simply round down minor bits of exponent and mantissa.
 struct half
 {
     uint16_t data;
 
+    half() : data(0) {}
     half(const half& v) : data(v.data) {}
     half(float v);
+    half& operator=(float v);
     float to_float() const;
 };
 
@@ -21,6 +25,7 @@ struct snorm16
     snorm16() : data(0) {}
     snorm16(const snorm16& v) : data(v.data) {}
     snorm16(float v) : data(uint16_t(v * 32767.0f)) {}
+    snorm16& operator=(float v);
     float to_float() const { return (float)data / 32767.0f; }
 
     static snorm16 zero() { return snorm16(0.0f); }
@@ -34,6 +39,7 @@ struct unorm16
     unorm16() : data(0) {}
     unorm16(const unorm16& v) : data(v.data) {}
     unorm16(float v) : data(uint16_t(v * 65535.0f)) {}
+    unorm16& operator=(float v);
     float to_float() const { return (float)data / 65535.0f; }
 
     static unorm16 zero() { return unorm16(0.0f); }
@@ -45,6 +51,9 @@ struct unorm16
 template<class T>
 struct tvec2
 {
+    using value_type = T;
+    static const int vector_size = 2;
+
     T data[2];
 
     T& operator[](size_t i) { return data[i]; }
@@ -54,6 +63,9 @@ struct tvec2
 template<class T>
 struct tvec3
 {
+    using value_type = T;
+    static const int vector_size = 3;
+
     T data[3];
 
     T& operator[](size_t i) { return data[i]; }
@@ -63,6 +75,9 @@ struct tvec3
 template<class T>
 struct tvec4
 {
+    using value_type = T;
+    static const int vector_size = 4;
+
     T data[4];
 
     T& operator[](size_t i) { return data[i]; }
@@ -84,6 +99,24 @@ using snorm16x4 = tvec4<snorm16>;
 using unorm16x2 = tvec2<unorm16>;
 using unorm16x3 = tvec3<unorm16>;
 using unorm16x4 = tvec4<unorm16>;
+
+
+float  to_float(float  v); // 
+float2 to_float(float2 v); // 
+float3 to_float(float3 v); // 
+float4 to_float(float4 v); // just return v. convenient for template functions
+float  to_float(half  v);
+float2 to_float(half2 v);
+float3 to_float(half3 v);
+float4 to_float(half4 v);
+float  to_float(snorm16   v);
+float2 to_float(snorm16x2 v);
+float3 to_float(snorm16x3 v);
+float4 to_float(snorm16x4 v);
+float  to_float(unorm16   v);
+float2 to_float(unorm16x2 v);
+float3 to_float(unorm16x3 v);
+float4 to_float(unorm16x4 v);
 
 } // namespace nvc
 
