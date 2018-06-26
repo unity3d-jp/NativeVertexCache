8321f460473a5c680ca8db3a834c03fb801bd355
Takayuki Matsuoka <takayuki@unity3d.com>
2018-06-26 22:35:43 +0200

    Add simple file writing benchmark

--- Plugin/NativeVertexCacheTest/TestFileStream.cpp
+++ Plugin/NativeVertexCacheTest/TestFileStream.cpp
@@ -6,8 +6,7 @@
 using namespace nvc;
 using ByteArray = std::vector<uint8_t>;
 
-void RunTest_FileStream()
-{
+static void test0() {
 	ByteArray ba(1024);
 //	FillRandom(ba, 0x1234, 0x4321);
 	FillSequence(ba);
@@ -58,4 +57,34 @@ void RunTest_FileStream()
 
 	assert(msSrc.getLength() == ba.size());
 	assert(msDst.getLength() == bb.size());
+}
+
+static void test1() {
+	ByteArray buf(1024*1024);
+	FillRandom(buf, 0x1234, 0x4321);
+	size_t nLoop = 1024;
+
+	const auto t0 = GetHighResolutionClock();
+	{
+		const char* filename = "fs_test_benchmark.file";
+		FileStream fs(
+			  filename
+			, FileStream::OpenModes::Random_ReadWrite
+		);
+
+		for(size_t iLoop = 0; iLoop < nLoop; ++iLoop) {
+			fs.write(buf.data(), buf.size());
+		}
+	}
+	const auto t1 = GetHighResolutionClock();
+
+	const auto dt = GetSeconds(t0, t1);
+	const auto totalBytes = (double) (buf.size() * nLoop);
+	printf("(%s): Sequential write speed is %f MB/sec\n", __FILE__, totalBytes / dt / 1024/1024);
+}
+
+void RunTest_FileStream()
+{
+	test0();
+	test1();
 }
