909c85a3874969569882df2320a9bbfa95331071
i-saint <saint.skr@gmail.com>
2018-06-27 18:32:09 +0200

    alembic: convert to monolithic vertex buffer (wip)

--- Plugin/AlembicToGeomCache/AlembicToGeomCache.h
+++ Plugin/AlembicToGeomCache/AlembicToGeomCache.h
@@ -3,6 +3,14 @@
 #include "../Types.h"
 #include "../nvcAPI.h"
 
+#define nvcSemantic_Points      "points"
+#define nvcSemantic_Velocities  "velocities"
+#define nvcSemantic_Normals     "normals"
+#define nvcSemantic_Tangents    "tangents"
+#define nvcSemantic_UV0         "uv0"
+#define nvcSemantic_UV1         "uv1"
+#define nvcSemantic_Colors      "colors"
+
 namespace nvcabc {
 
 struct AlembicImportOptions
--- Plugin/NativeVertexCacheTest/TestAlembic.cpp
+++ Plugin/NativeVertexCacheTest/TestAlembic.cpp
@@ -1,5 +1,8 @@
 #include "Plugin/PrecompiledHeader.h"
 #include "Plugin/AlembicToGeomCache/AlembicToGeomCache.h"
+#include "Plugin/NativeVertexCacheTest/TestUtil.h"
+#include "Plugin/InputGeomCache.h"
+
 
 void RunTest_Alembic()
 {
@@ -8,7 +11,12 @@ void RunTest_Alembic()
     AlembicImportOptions opt;
     AlembicGeometries result;
 
-    if (AlembicToGeomCache("Cloth.abc", opt, result)) {
+    const char *path_to_abc = "Cloth.abc";
 
+    if (AlembicToGeomCache(path_to_abc, opt, result)) {
+        printf("%s %d time samples\n", path_to_abc, (int)result.timesamples.size());
+    }
+    else {
+        ThrowError("failed to open %s\n", path_to_abc);
     }
 }
