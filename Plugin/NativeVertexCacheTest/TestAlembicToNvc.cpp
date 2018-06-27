81c625120a5384d7870b84182a1d4dd138c573a2
Takayuki Matsuoka <takayuki@unity3d.com>
2018-06-27 17:06:00 +0200

    Add pesudo .abc to .nvc converter for testing (WIP)
    It doesn't work for now.

--- Plugin/NativeVertexCacheTest/TestAlembicToNvc.cpp
+++ Plugin/NativeVertexCacheTest/TestAlembicToNvc.cpp
@@ -1,14 +1,27 @@
 #include "Plugin/PrecompiledHeader.h"
 #include "Plugin/AlembicToGeomCache/AlembicToGeomCache.h"
+#include "Plugin/Stream/FileStream.h"
+#include "Plugin/Compression/NullCompressor.h"
+#include "Plugin/NativeVertexCacheTest/TestUtil.h"
 
 void RunTest_AlembicToNvc()
 {
+    using namespace nvc;
     using namespace nvcabc;
 
+    const char* abcFilename = "../../../Data/Cloth-1frame.abc";
+    const char* nvcFilename = "../../../Data/Cloth-1frame.nvc";
+
+    assert(IsFileExist(abcFilename));
+    RemoveFile(nvcFilename);
+
     AlembicImportOptions opt;
-    AlembicGeometries result;
+    AlembicGeometries abcGeoms;
 
-    if (AlembicToGeomCache("Cloth.abc", opt, result)) {
+    const auto abcToGcResult = AlembicToGeomCache(abcFilename, opt, abcGeoms);
+    assert(abcToGcResult);
 
-    }
+    FileStream fs { nvcFilename, FileStream::OpenModes::Random_ReadWrite };
+    NullCompressor nc {};
+    nc.compress(*abcGeoms.geometry, &fs);
 }
